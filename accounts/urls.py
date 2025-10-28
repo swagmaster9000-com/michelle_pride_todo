44d234c5912b7e1fb2743f0a50d4c5231e7b0eac
Michelle <ampride@memphis.edu>
2025-10-27 19:23:35 -0500

    Initial commit: Django todo project Merge branch 'main' of github.com:swagmaster9000-com/venv

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,24 +1,15 @@
 from django.urls import path
-from . import views
 from django.contrib.auth import views as auth_views
+from . import views
+
+
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-
-    path('password_reset/',
-         auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
-         name='password_reset'),
-
-    path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
-         name='password_reset_done'),
-
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
-         name='password_reset_confirm'),
-
-    path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
-         name='password_reset_complete'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 ]
+
